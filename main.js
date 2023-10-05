de867d5bb7aec7294ca1b05681dc6f5c125c6e68
mastercodergautam <125134265+mastercodergautam@users.noreply.github.com>
2023-10-05 14:57:22 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+//https://teachablemachine.withgoogle.com/models/JnItbQnqV/
+Webcam.set({
+    width:350,
+    height:300,
+    image_format : 'png',
+    png_quality:90
+
+});
+camera=document.getElementById("camera");
+Webcam.attach('#camera')
+
+
+console.log('ml5 version',ml5.version);
+classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/JnItbQnqV/model.json',modelLoded);
+function modelLoded(){
+    console.log("modelloded")
+    Webcam.snap(function(data_uri){
+        document.getElementById("camera").innerHTML='<img id="captured_image" src='+data_uri+'>';
+    })
+    img=document.getElementById("captured_image") 
+    classifier.classify(img,gotresults);
+    
+}
+
+
+function gotresults(error,results){
+    if(error){
+        console.log(error)
+    }
+    else{
+        console.log(results);
+        document.getElementById("result_object_name").innerHTML=results[0].label;
+        document.getElementById("result_object_accuracy").innerHTML=results[0].confidence.toFixed(3);
+        
+       
+    }
+}
